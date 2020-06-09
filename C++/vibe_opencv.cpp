363b74debb1b09810f5e82104a48c612abb1a73e
zdhuang <zdhuang@aibee.com>
2020-06-09 18:26:16 +0800

    init

---
+++ C++/vibe_opencv.cpp
@@ -0,0 +1,162 @@
+/* Copyright - Benjamin Laugraud <blaugraud@ulg.ac.be> - 2016
+ * Copyright - Marc Van Droogenbroeck <m.vandroogenbroeck@ulg.ac.be> - 2016
+ *
+ * ViBe is covered by a patent (see http://www.telecom.ulg.ac.be/research/vibe).
+ *
+ * Permission to use ViBe without payment of fee is granted for nonprofit
+ * educational and research purposes only.
+ *
+ * This work may not be copied or reproduced in whole or in part for any
+ * purpose.
+ *
+ * Copying, reproduction, or republishing for any purpose shall require a
+ * license. Please contact the authors in such cases. All the code is provided
+ * without any guarantee.
+ *
+ * This simple example program takes a path to a video sequence as an argument.
+ * When it is executed, two windows are opened: one displaying the input
+ * sequence, and one displaying the segmentation maps produced by ViBe. Note
+ * that this program uses the a polychromatic version of ViBe with 3 channels.
+ */
+#include <cstddef>
+#include <ctime>
+#include <iostream>
+#include <json/json.h>
+
+#ifndef    OPENCV_3
+#include <cv.h>
+#include <highgui.h>
+#else
+#include <opencv2/opencv.hpp>
+#endif  /* OPENCV_3 */
+
+#include <libvibe++/ViBe.h>
+#include <libvibe++/distances/Manhattan.h>
+#include <libvibe++/system/types.h>
+
+using namespace std;
+using namespace cv;
+using namespace ViBe;
+
+#define VIS_VID  0
+#define VIS_IMG 0
+
+
+int main(int argc, char** argv) {
+  if (argc <  3) {
+    cerr << "Usage: " << argv[0] << " video_name  json_name frames(option)" << endl;
+    return EXIT_FAILURE;
+  }
+
+  int max_print_frames = -1;
+  if (argc == 4) {
+      max_print_frames = std::atoi(argv[3]);
+  }
+
+  /* Parameterization of ViBe. */
+  typedef ViBeSequential<3, Manhattan<3> >                                ViBe;
+
+  /* Random seed. */
+  srand(time(NULL));
+
+  cv::VideoCapture decoder(argv[1]);
+  cv::Mat origin_frame;
+
+  float scale = 1.0;
+  int32_t height = (int) (scale * decoder.get(CV_CAP_PROP_FRAME_HEIGHT));
+  int32_t width  = (int) (scale * decoder.get(CV_CAP_PROP_FRAME_WIDTH));
+
+#ifdef VIS_VID
+  string outputVideoPath = "./ccb_airport_mask/airport.avi";  
+  cv::VideoWriter outputVideo;
+  cv::Size S = cv::Size((int)width,
+		        (int)height);
+  double r = decoder.get(CV_CAP_PROP_FPS);
+  outputVideo.open(outputVideoPath, CV_FOURCC('M', 'J', 'P', 'G'), r, S, true);
+#endif
+
+  ViBe* vibe = NULL;
+  bool firstFrame = true;
+  int index_frame = 1;
+  cv::Mat segmentationMap(height, width, CV_8UC1);
+  cv::Mat frame(height, width, CV_8UC3); 
+  Json::Value res_json;
+ 
+  while (decoder.read(origin_frame)) {
+    cv::resize(origin_frame, frame, cv::Size(width, height));
+    if (firstFrame) {
+      /* Instantiation of ViBe. */
+      vibe = new ViBe(height, width, frame.data);
+      firstFrame = false;
+    }
+
+    /* Segmentation and update. */
+    vibe->segmentation(frame.data, segmentationMap.data);
+    vibe->update(frame.data, segmentationMap.data);
+
+    /* Post-processing: 3x3 median filter. */
+    medianBlur(segmentationMap, segmentationMap, 3);
+
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    findContours(segmentationMap, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+    vector<Rect> boundRect(contours.size());
+    vector<vector<Point> > contours_poly( contours.size() );
+    for(unsigned int i = 0; i< contours.size(); i++ )
+    {
+	approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+	boundRect[i] = boundingRect(Mat(contours_poly[i]) );
+    }
+
+    Json::Value frame_boxes;
+    for(unsigned int i = 0; i< contours.size(); i++ )
+    {
+        Scalar color = Scalar(0, 0, 255);
+	if (boundRect[i].area() > 50) {  
+#ifdef VIS_VID 
+	    rectangle(frame, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+#endif
+            Json::Value box;
+	    box[0] = boundRect[i].x;
+	    box[1] = boundRect[i].y;
+	    box[2] = boundRect[i].width;
+	    box[3] = boundRect[i].height;
+	    frame_boxes.append(box);
+	}
+    }
+
+    res_json[std::to_string(index_frame)] = frame_boxes;
+    
+#ifdef VIS_VID 
+    cv::Mat res_map;
+    cv::cvtColor(segmentationMap, res_map, COLOR_GRAY2BGR);
+    //outputVideo.write(segmentationMap);
+    //outputVideo.write(res_map);
+    outputVideo.write(frame);
+#endif
+
+#ifdef VIS_IMG
+    cv::imwrite("./ccb_airport_mask/airport_" + std::to_string(index_frame) + ".jpg", segmentationMap);
+#endif
+
+    if (index_frame % 100 == 0) { 
+    	cout << "process_frame_index: " << index_frame << endl;
+    }
+
+    index_frame += 1;
+    if (index_frame > max_print_frames && max_print_frames > 0) {
+	break;
+    }
+
+  }
+
+  std::ofstream out(argv[2]);
+  out << res_json;
+
+  delete vibe;
+
+  decoder.release();
+
+  return EXIT_SUCCESS;
+}
---
+++ python/multi_thread_crop.py
@@ -0,0 +1,88 @@
+import os
+import cv2
+import queue
+import tqdm
+import threading
+
+
+THREAD_NUM = 10
+queueLock = threading.Lock()
+mkdir_lock = threading.Lock()
+
+dest_folder = 'crop_image_path'
+prefix_len = len('path_prefix')
+
+
+class ProcessThread(threading.Thread):
+    def __init__(self, thread_id, q, func):
+        threading.Thread.__init__(self)
+        self.threadID = thread_id
+        self.q = q
+        self.func = func
+
+    def run(self):
+        print("thread %d start" % self.threadID)
+        process_center_crop(self.threadID, self.q, self.func)
+        print("thread %d end" % self.threadID)
+
+
+def process_center_crop(thread_id, queue, func):
+    while not queue.empty():
+        queueLock.acquire()
+        if not queue.empty():
+            line = queue.get()
+            queueLock.release()
+            func(line)
+        else:
+            queueLock.release()
+
+
+def start_group_threads(lines, process_func):
+    thread_list = []
+    imgs_queue = queue.Queue()
+
+    queueLock.acquire()
+    for line in tqdm.tqdm(lines):
+        imgs_queue.put(line)
+    queueLock.release()
+
+    for threadID in range(1, THREAD_NUM + 1):
+        thread = ProcessThread(threadID, imgs_queue, process_func)
+        thread.start()
+        thread_list.append(thread)
+
+    for t in thread_list:
+        t.join()
+
+    print("exist main Thread")
+
+
+def center_crop_core(line):
+    face_img_file = line.split(' ')[0]
+    img = cv2.imread(face_img_file)
+    img = img[50:250, 50:250]
+    img = cv2.resize(img, (128, 128))
+    dst_file_name = os.path.basename(face_img_file)
+    behind_name = face_img_file[prefix_len:]
+    middle_name = behind_name[:-len(dst_file_name)]
+    prefix_path = os.path.join(dest_folder, middle_name)
+    mkdir_lock.acquire()
+    if not os.path.exists(prefix_path):
+        os.mkdir(prefix_path)
+    mkdir_lock.release()
+    whole_file_name = os.path.join(prefix_path, dst_file_name)
+    cv2.imwrite(whole_file_name, img)
+
+
+def center_crop(face_id_file):
+    with open(face_id_file) as face_fl:
+        lines = face_fl.readlines()
+        start_group_threads(lines, center_crop_core)
+
+
+if __name__ == '__main__':
+    train_list = 'train_list.list'
+    if not os.path.exists(dest_folder):
+        os.mkdir(dest_folder)
+    center_crop(wanda_train_list)
+
