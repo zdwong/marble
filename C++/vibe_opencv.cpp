ef70cb75222144a8468333bc5708719e83eb1204
Mark <1241224798@qq.com>
2020-06-09 18:34:05 +0800

    Update vibe_opencv.cpp

--- C++/vibe_opencv.cpp
+++ C++/vibe_opencv.cpp
@@ -1,23 +1,3 @@
-/* Copyright - Benjamin Laugraud <blaugraud@ulg.ac.be> - 2016
- * Copyright - Marc Van Droogenbroeck <m.vandroogenbroeck@ulg.ac.be> - 2016
- *
- * ViBe is covered by a patent (see http://www.telecom.ulg.ac.be/research/vibe).
- *
- * Permission to use ViBe without payment of fee is granted for nonprofit
- * educational and research purposes only.
- *
- * This work may not be copied or reproduced in whole or in part for any
- * purpose.
- *
- * Copying, reproduction, or republishing for any purpose shall require a
- * license. Please contact the authors in such cases. All the code is provided
- * without any guarantee.
- *
- * This simple example program takes a path to a video sequence as an argument.
- * When it is executed, two windows are opened: one displaying the input
- * sequence, and one displaying the segmentation maps produced by ViBe. Note
- * that this program uses the a polychromatic version of ViBe with 3 channels.
- */
 #include <cstddef>
 #include <ctime>
 #include <iostream>
